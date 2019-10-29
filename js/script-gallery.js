dc769bc4cf5f3458dda173ecbbc2c911a754cd16
Robert Flunger <robert.flunger@gmx.at>
2019-10-29 15:59:44 +0100

    yes

--- js/script-gallery.js
+++ js/script-gallery.js
@@ -1,39 +1,17 @@
 
 var pics = JSON.parse(photos);
-console.log(Object.values(pics) == "akt");
 
-
-
-/*console.time("test");
-function appendToHtml(object, callback) {
-
-var html = "";
-for (let i=0; i<pics.length; i++) {
-	html += `<div class="grid-item col-sm-6 col-md-4"><div class="grid-item-content"><img src="img/${pics[i].file}"></div></div>`;
-}
-
-document.getElementById("test").innerHTML += html;
-setTimeout(function() {callback();}, 100);
-}
-
-function loadMasonry () {
-
-$('.grid').masonry({
-		itemSelector: '.grid-item',
-		columnWidth: '.grid-sizer',
-		percentPosition: true,
-		transitionDuration: '0.2s'
-	})
-}
-
-appendToHtml(pics, loadMasonry);
-*/
+$('body').click(function (event) {
+	if(!$(event.target).closest('#myModal').length && !$(event.target).is('#myModal')) {
+		$()
+	}
+})
 
 function appendToHtml(object, callback) {
 
 	var html = "";
 	for (let i=0; i<pics.length; i++) {
-		html += `<div class="grid-item col-sm-6 col-md-4"><div class="grid-item-content"><img src="img/${pics[i].file}"></div></div>`;
+		html += `<div class="grid-item col-sm-6 col-md-4" onclick="showImage('../img/${pics[i].file}')"><div class="grid-item-content"><img src="../img/${pics[i].file}"></div></div>`;
 	}
 	console.log(html);
 	document.getElementById("gallery-container").innerHTML += html;
@@ -79,6 +57,31 @@ if (document.getElementById("portrait")) {
 }
 
 
+
+var modal = document.getElementById("modal-root");
+var modalImg = document.getElementById("image");
+
+function showImage(image) {
+	modal.style.display = "block";
+	modalImg.src = image;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+// document.getElementById("modal-root").addEventListener('click', rootclick, false);
+document.querySelector("#modal-root:not(#image)").addEventListener('click', rootclick, true);
+
+/*$("#modal-root").not("#image").addEventListener('click', rootclick)*/;
+
+function rootclick() {
+	modal.style.display = "none";
+}
+
 /*
 setTimeout("loadMasonry()", 100);*/
 
@@ -86,3 +89,29 @@ setTimeout("loadMasonry()", 100);*/
 // $(document).ready(function(){})
 // document.readystate
 /*maybe generator function? see: https://www.freecodecamp.org/news/javascript-from-callbacks-to-async-await-1cc090ddad99/*/
+
+
+/*console.time("test");
+function appendToHtml(object, callback) {
+
+var html = "";
+for (let i=0; i<pics.length; i++) {
+	html += `<div class="grid-item col-sm-6 col-md-4"><div class="grid-item-content"><img src="img/${pics[i].file}"></div></div>`;
+}
+
+document.getElementById("test").innerHTML += html;
+setTimeout(function() {callback();}, 100);
+}
+
+function loadMasonry () {
+
+$('.grid').masonry({
+		itemSelector: '.grid-item',
+		columnWidth: '.grid-sizer',
+		percentPosition: true,
+		transitionDuration: '0.2s'
+	})
+}
+
+appendToHtml(pics, loadMasonry);
+*/
